0ec706c13560d936885f8f7ae369896cb7095e31
Kyle Johnston <kjohnston@Administrators-MacBook-Pro-2.local>
2015-11-10 22:36:31 -0600

    Changed 'donate' to 'contribute'

--- donate.php
+++ donate.php
@@ -4,10 +4,10 @@
 		<?php include('includes/analytics.php'); ?>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Donate to Reggie's campaign for US Congress">
-		<meta name="Keywords" content="Donate, Reggie Marselus, Kansas, Congress, 3rd District, Johnson County">
+		<meta name="description" content="Contribute to Reggie's campaign for US Congress">
+		<meta name="Keywords" content="Contribute, Donate, Reggie Marselus, Kansas, Congress, 3rd District, Johnson County">
 		<meta name="Robots" content="index,follow" />
-		<title>Donate | Reggie for Congress - Kansas 3rd District</title>
+		<title>Contribute | Reggie for Congress - Kansas 3rd District</title>
 		<?php include('includes/head.php'); ?>
 	</head>
 	<body>
@@ -17,9 +17,9 @@
 			</div><!-- #page-nav -->
 
 			<div class="row primary">
-				<h1 class="large-12 columns page-title">Donate &#8211; Support the Campaign</h1>
+				<h1 class="large-12 columns page-title">Contribute &#8211; Support the Campaign</h1>
 				<div class="medium-12 columns">
-					<h3 class="title">Mail Your Donation</h3>
+					<h3 class="title">Mail Your Contribution</h3>
 					<p>Payable to &ldquo;Reggie for Congress&rdquo;<br>
 					PO Box 14826<br>
 					Lenexa, KS 66285</p>
